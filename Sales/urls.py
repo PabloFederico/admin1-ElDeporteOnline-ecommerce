2b9512765a7f2dc57e0ed89549a085769d713126
Matías Bataglia <matubat91@gmail.com>
2020-12-07 19:47:41 -0300

    Se guarda la venta

--- Sales/urls.py
+++ Sales/urls.py
@@ -6,5 +6,5 @@ from .views.cart_view import AddToCartView, RemoveFromCartView
 urlpatterns = [
     path('agregar-al-carrito', AddToCartView.as_view(), name='add_to_cart'),
     path('sacar-del-carrito', RemoveFromCartView.as_view(), name='remove_from_cart'),
-    path('comprar', BuyView.as_view(), name='buy'),
+    path('comprar', BuyView.as_view(), name='buy')
 ]
--- Sales/views/buy_view.py
+++ Sales/views/buy_view.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.views import View
 
+from Products.models import Product
+from Sales.models import Cart, Sale, Item
+
 
 class BuyView(View):
 
@@ -10,4 +13,16 @@ class BuyView(View):
     def post(self, request):
         # aca hay que recibir la compra y guardarla en la db
         # tambien hay que vaciar el carrito
-        pass
+        cart = Cart(request)
+        items = []
+        sale = Sale.objects.create()
+        for entry in cart.get_products():
+            product = Product.objects.get(pk=entry.get('product').id)
+            item = Item.objects.create(sale=sale,product=product, quantity=entry.get('quantity'))
+            items.append(item)
+
+        sale.items.set(items)
+
+        # sale.save()
+        cart.clear()
+        return render(request, 'sales/buy-complete.html')
