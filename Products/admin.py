8a7d390889aa14e5c7c02cb3b7cd295e9c82e60e
Matias Feld <feldmatias@gmail.com>
2020-12-09 16:16:45 -0300

    abm variantes de productos

--- Products/admin.py
+++ Products/admin.py
@@ -5,17 +5,18 @@ from nested_inline.admin import NestedModelAdmin, NestedStackedInline, NestedTab
 from Products.models import Product, ProductVariant, ProductVariantValue, ProductImage
 
 
-# class ProductVariantValueInline(NestedTabularInline):
-#     model = ProductVariantValue
-#     extra = 1
-#     fk_name = 'variant'
-#
-#
-# class ProductVariantInline(NestedStackedInline):
-#     model = ProductVariant
-#     extra = 1
-#     fk_name = 'product'
-#     inlines = [ProductVariantValueInline]
+class ProductVariantValueInline(NestedTabularInline):
+    model = ProductVariantValue
+    extra = 1
+    fk_name = 'variant'
+
+
+class ProductVariantInline(NestedStackedInline):
+    model = ProductVariant
+    extra = 1
+    fk_name = 'product'
+    inlines = [ProductVariantValueInline]
+
 
 class ProductImageInline(NestedStackedInline):
     model = ProductImage
@@ -27,10 +28,7 @@ class ProductAdmin(NestedModelAdmin):
     list_display = ["name", "short_description", "price", "image_preview"]
     search_fields = ['name']
     model = Product
-    inlines = [ProductImageInline]
-    # inlines = [ProductVariantInline]  # TODO: enable when product variants are implemented
+    inlines = [ProductImageInline, ProductVariantInline]
 
     def image_preview(self, product):
         return mark_safe(f'<img src="{product.cover_image()}" width="100" height="100" />')
-
-
