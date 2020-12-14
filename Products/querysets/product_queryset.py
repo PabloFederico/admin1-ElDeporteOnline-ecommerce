e87153029025b2173cbf5500021d36a32c34d378
lucas <lhemmingsen@fi.uba.ar>
2020-12-13 23:59:51 -0300

    Ordenar precios con descuentos

--- Products/querysets/product_queryset.py
+++ Products/querysets/product_queryset.py
@@ -1,14 +1,15 @@
 from django.db import models
+from django.db.models import F, ExpressionWrapper, DecimalField
 
 
 class ProductQuerySet(models.QuerySet):
 
     def for_catalog(self):
-        return self.all()
+        return self.all().annotate(price_sale = ExpressionWrapper(F('price')*(100 - F('discount'))/100, output_field=DecimalField(decimal_places=2)))
 
     def order_by_price(self, order):
         if not order:
             return self
 
         sign = "" if order == "asc" else "-"
-        return self.order_by(sign + 'price_currency', sign + 'price')
+        return self.order_by(sign + 'price_currency', sign + 'price_sale')
